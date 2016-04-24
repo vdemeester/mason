67bf6372ce4135ff988e61e64e3f63e1c7fa5b2e
Vincent Demeester <vincent@sbr.pm>
2016-04-24 21:57:34 +0200

    Fixing validate thingies
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- dockerfile/steps.go
+++ dockerfile/steps.go
@@ -14,6 +14,7 @@ import (
 	"github.com/vdemeester/mason/builder"
 )
 
+// LabelStep is a step for LABEL.
 type LabelStep struct {
 	labels map[string]string
 }
@@ -22,6 +23,7 @@ func (s *LabelStep) String() string {
 	return fmt.Sprintf("LABEL %v", s.labels)
 }
 
+// Execute implements Step.Execute. It executes the step based on the specified config and helper.
 func (s *LabelStep) Execute(ctx context.Context, helper base.Helper, config *builder.Config) (*builder.Config, error) {
 	c, err := helper.ContainerCreate(ctx, types.ContainerCreateConfig{
 		Config: &container.Config{
@@ -37,6 +39,7 @@ func (s *LabelStep) Execute(ctx context.Context, helper base.Helper, config *bui
 	return config, nil
 }
 
+// RunStep is a step for RUN.
 type RunStep struct {
 	heredoc string
 }
@@ -45,6 +48,7 @@ func (s *RunStep) String() string {
 	return fmt.Sprintf("RUN %v", s.heredoc)
 }
 
+// Execute implements Step.Execute. It executes the step based on the specified config and helper.
 func (s *RunStep) Execute(ctx context.Context, helper base.Helper, config *builder.Config) (*builder.Config, error) {
 	containerID, ok := config.Get(builder.ContainerID)
 	if !ok {
@@ -68,6 +72,7 @@ func (s *RunStep) Execute(ctx context.Context, helper base.Helper, config *build
 	return config, nil
 }
 
+// CopyStep is a step for COPY.
 type CopyStep struct {
 	srcPath     string
 	destPath    string
@@ -78,6 +83,7 @@ func (s *CopyStep) String() string {
 	return fmt.Sprintf("COPY %s %s", s.srcPath, s.destPath)
 }
 
+// Execute implements Step.Execute. It executes the step based on the specified config and helper.
 func (s *CopyStep) Execute(ctx context.Context, helper base.Helper, config *builder.Config) (*builder.Config, error) {
 	containerID, ok := config.Get(builder.ContainerID)
 	if !ok {
